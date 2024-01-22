c00c3e3536efc305a097403e94af605fe37e26ce
nickdizes <nick.dizes18@gmail.com>
2024-01-22 13:34:57 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//user enters a quote
+//how many words
+//how many characters
+//how many of each letter was used
+
+using OOPFun;
+
+//classes
+WordTools wt = new WordTools();
+
+//variables
+string quote = "";
+
+//Gathering user input
+Console.WriteLine("Please enter a quote to analyze: ");
+quote = Console.ReadLine();
+
+Console.WriteLine("Number of Words: " + wt.WordCounter(quote));
+Console.WriteLine("Number of Characters: " + wt.CharacterCounter(quote));
+Console.WriteLine("Alpahabet: ");
+wt.AlphabetCounter(quote);
---
+++ WordTools.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPFun
+{
+    internal class WordTools
+    {
+        public int WordCounter(string words)
+        {
+            //variables
+            int wordCount = 0;
+
+            //split the string into seperate words to make it easier to count
+            string[] individualWords = words.Split(" ");
+
+            wordCount = individualWords.Length;
+
+            return (wordCount);
+        }
+
+        public int CharacterCounter(string words)
+        {
+            int characterCount = 0;
+
+            characterCount = words.Length;
+
+            return (characterCount);
+        }
+
+        public void AlphabetCounter(string words)
+        { 
+            int[] letters = new int[26]; //holds the count of each letter (a-z)
+
+            //Loop throught the entire string
+            for(int i = 0; i < words.Length; i++)
+            {
+               
+
+                char c = words[i];
+
+                c = Char.ToLower(c);
+
+                if (c >= 97 && c <= 122) //check to make sure it is a lower case letter
+                {
+                    letters[(c - 97)]++;
+                }
+            }
+
+            //Print Results from the array
+            for(int i = 0; i < letters.Length; i++)
+            {
+                char letter = (char)(i + 97);
+
+                Console.WriteLine(letter + ": " + letters[i]);
+            }
+        }
+    }
+}
